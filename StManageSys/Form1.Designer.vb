8f3f057216a297df2115f609bd1ebfee9b72d863
unknown <bappi@tosei-japan.com>
2023-10-18 14:18:11 +0900

    First commit

---
+++ StManageSys/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxFName = New System.Windows.Forms.TextBox()
+        Me.TextBoxLName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxAddress = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.TextBoxSearch = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(119, 146)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FristName"
+        '
+        'TextBoxFName
+        '
+        Me.TextBoxFName.Location = New System.Drawing.Point(239, 146)
+        Me.TextBoxFName.Name = "TextBoxFName"
+        Me.TextBoxFName.Size = New System.Drawing.Size(170, 22)
+        Me.TextBoxFName.TabIndex = 1
+        '
+        'TextBoxLName
+        '
+        Me.TextBoxLName.Location = New System.Drawing.Point(239, 193)
+        Me.TextBoxLName.Name = "TextBoxLName"
+        Me.TextBoxLName.Size = New System.Drawing.Size(170, 22)
+        Me.TextBoxLName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(119, 199)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "LastName"
+        '
+        'TextBoxAddress
+        '
+        Me.TextBoxAddress.Location = New System.Drawing.Point(239, 239)
+        Me.TextBoxAddress.Name = "TextBoxAddress"
+        Me.TextBoxAddress.Size = New System.Drawing.Size(170, 22)
+        Me.TextBoxAddress.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(119, 239)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Address"
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(776, 100)
+        Me.Panel1.TabIndex = 6
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(122, 296)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 7
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(219, 296)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 8
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(334, 296)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 9
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'TextBoxSearch
+        '
+        Me.TextBoxSearch.Location = New System.Drawing.Point(571, 402)
+        Me.TextBoxSearch.Name = "TextBoxSearch"
+        Me.TextBoxSearch.Size = New System.Drawing.Size(208, 22)
+        Me.TextBoxSearch.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(510, 405)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 16)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Search"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxSearch)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBoxAddress)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxLName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxFName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "StudentsFrom"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxFName As TextBox
+    Friend WithEvents TextBoxLName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxAddress As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents TextBoxSearch As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ StManageSys/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim conn As New SqlConnection("Data Source=TOSEI-012\SQLEXPRESS; Initial Catalog=StudentsDB; Integrated Security=True")
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub ExecuteQuery(ByVal query As String)
+        Dim cmd As New SqlCommand(query, conn)
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim insertquery As String = " INSERT INTO StInfoTbl(FIRSTNAME, LASTNAME, ADDRESS) VALUES(
+        '" & TextBoxFName.Text & "' ,
+        '" & TextBoxLName.Text & "' ,
+        '" & TextBoxAddress.Text & "' 
+        ) "
+        ExecuteQuery(insertquery)
+        MessageBox.Show("Record inserted suceessfully", "INSERT", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ClearEntry()
+    End Sub
+
+    Private Sub TextBoxSearch_TextChanged(sender As Object, e As EventArgs) Handles TextBoxSearch.TextChanged
+        Dim cmd As New SqlCommand("SELECT * FROM StInfoTbl WHERE ID=@ID", conn)
+        cmd.Parameters.AddWithValue("@ID", SqlDbType.Int).Value = TextBoxSearch.Text
+        Dim DataAdapter As New SqlDataAdapter(cmd)
+        Dim ShowDataOnTable As New DataTable
+        DataAdapter.Fill(ShowDataOnTable)
+
+        If ShowDataOnTable.Rows.Count > 0 Then
+            TextBoxFName.Text = ShowDataOnTable.Rows(0)(1).ToString
+            TextBoxLName.Text = ShowDataOnTable.Rows(0)(2).ToString
+            TextBoxAddress.Text = ShowDataOnTable.Rows(0)(3).ToString
+        Else
+            MessageBox.Show("No record found", "No record", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim updatequery As String = "UPDATE StInfoTbl SET 
+
+        FIRSTNAME= '" & TextBoxFName.Text & "', 
+        LASTNAME = '" & TextBoxLName.Text & "',
+        ADDRESS = '" & TextBoxAddress.Text & "'
+        WHERE ID = '" & TextBoxSearch.Text & "'"
+        ExecuteQuery(updatequery)
+        MessageBox.Show("Record updated successfuly ", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ClearEntry()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim deletequery As String = "DELETE FROM StInfoTbl WHERE ID= '" & TextBoxSearch.Text & "'"
+        ExecuteQuery(deletequery)
+        MessageBox.Show("Record deleted successfuly ", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ClearEntry()
+    End Sub
+
+    Public Sub ClearEntry()
+        TextBoxFName.Clear()
+        TextBoxLName.Clear()
+        TextBoxAddress.Clear()
+    End Sub
+
+
+End Class
